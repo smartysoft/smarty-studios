ed29acf44e471058c68464137b76da7ab8b34b12
Ozan Manav <smarty.yazilim@gmail.com>
2025-05-30 14:29:28 +0300

    fixes

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,12 +1,6 @@
 import { cn } from "../lib/utils";
 
-export const Logo = ({
-  className,
-  uniColor,
-}: {
-  className?: string;
-  uniColor?: boolean;
-}) => {
+export const Logo = ({ className }: { className?: string }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
