1b2ebd25231133d933820c20d9c90a77a76d5210
Ozan Manav <smarty.yazilim@gmail.com>
2025-05-31 12:37:16 +0300

    form

--- src/components/main-section.tsx
+++ src/components/main-section.tsx
@@ -109,22 +109,11 @@ export default function HeroSection() {
                       size="lg"
                       className="rounded-xl px-5 text-base"
                     >
-                      <Link href="#link">
+                      <Link href="/estimate-project">
                         <span className="text-nowrap">Start Building</span>
                       </Link>
                     </Button>
                   </div>
-                  <Button
-                    key={2}
-                    asChild
-                    size="lg"
-                    variant="ghost"
-                    className="h-10.5 rounded-xl px-5"
-                  >
-                    <Link href="#link">
-                      <span className="text-nowrap">Request a demo</span>
-                    </Link>
-                  </Button>
                 </AnimatedGroup>
               </div>
             </div>
